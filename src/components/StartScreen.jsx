eabe42b6f017f2a7018b81de12111f53b1a3f62b
Urion <bernardo66@live.com>
2023-08-22 15:50:30 -0300

    Secret_Word

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './StartScreen.css'
+
+const StartScreen = ({startGame}) => {
+  return (
+    <div >
+        <h1>Secret Word</h1>
+        <p className='cor'>Clique no botão abaixo para começar a jogar</p>
+        <button onClick={startGame}>Começar o jogo</button>
+        
+    </div>
+    
+  )
+}
+
+export default StartScreen
